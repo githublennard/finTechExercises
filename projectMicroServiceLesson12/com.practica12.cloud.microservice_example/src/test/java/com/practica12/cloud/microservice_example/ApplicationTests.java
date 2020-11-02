b578667857d9fce3eec66c13ab8dd12d7052b53d
developerworldwide <lennardwilde@gmail.com>
2020-11-02 22:30:03 +0100

    primer microservicio activo

---
+++ projectMicroServiceLesson12/com.practica12.cloud.microservice_example/src/test/java/com/practica12/cloud/microservice_example/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica12.cloud.microservice_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
