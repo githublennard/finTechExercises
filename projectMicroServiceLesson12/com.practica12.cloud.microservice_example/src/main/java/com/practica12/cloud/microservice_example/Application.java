a4aa412bfea0f5c06e74a281f47376a79b50b558
developerworldwide <lennardwilde@gmail.com>
2020-11-10 00:51:36 +0100

    update microservice for this stage

--- projectMicroServiceLesson12/com.practica12.cloud.microservice_example/src/main/java/com/practica12/cloud/microservice_example/Application.java
+++ projectMicroServiceLesson12/com.practica12.cloud.microservice_example/src/main/java/com/practica12/cloud/microservice_example/Application.java
@@ -6,21 +6,93 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
 @SpringBootApplication
 @RestController
+@Configuration
+@EnableAutoConfiguration
+@EnableConfigurationProperties(MyProps.class)
+@RefreshScope
+
+//@EnableEurekaClient
 
 public class Application {
+
+	@Autowired
+	private Service service;
+
+	@Autowired
+	private MyProps props;
+
+	@Value("${myprops.alias:alias!}")
+	String alias;
+
+	@Bean
+	@RefreshScope
+	public Service service()
+	{
+		return new Service();
+		//return new Service(props.getName());
+	}
 
 	@RequestMapping("/")
 	public String home()
 	{
-		//return "Hello " + service.getName() + "!" + " with alias " + alias;
-		return "Hello World!";
+		return "Hello " + service.getName() + "!" + " with alias " + alias;
+		//return "Hello World!";
 	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
-
 }
+
+@Data
+@RequiredArgsConstructor
+class Service
+{
+	private final String name;
+
+	//private Service()
+	Service()
+	{
+		name = "UNKNOWN";
+	}
+
+	public String getName() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
+
+@Data
+@ConfigurationProperties("myprops")
+class MyProps
+{
+	private String name = "World";
+
+	public Object getName() {
+		// TODO Auto-generated method stub
+		//return null;
+		return this.name;
+	}
+}
+
