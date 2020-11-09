6398f4a23a1282847188c0e2fab6e31a56191489
developerworldwide <lennardwilde@gmail.com>
2020-11-09 11:56:45 +0100

    app con soporte spring cloud

--- projectConfigServerLesson12/com.practica12.cloud.config_server/src/main/java/com/practica12/cloud/config_server/Application.java
+++ projectConfigServerLesson12/com.practica12.cloud.config_server/src/main/java/com/practica12/cloud/config_server/Application.java
@@ -2,8 +2,13 @@ package com.practica12.cloud.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@EnableConfigServer
+@Configuration
+
 public class Application {
 
 	public static void main(String[] args) {
