289138ad6c81ee6a3308e6832152fb03f052fd71
developerworldwide <lennardwilde@gmail.com>
2020-11-10 23:57:45 +0100

    update .jar and Application.java

--- projectConfigServerLesson12/com.practica12.cloud.config_server/src/main/java/com/practica12/cloud/config_server/Application.java
+++ projectConfigServerLesson12/com.practica12.cloud.config_server/src/main/java/com/practica12/cloud/config_server/Application.java
@@ -2,14 +2,24 @@ package com.practica12.cloud.config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 @EnableConfigServer
 @Configuration
 
-public class Application {
+public class Application implements ErrorController {
+	@Override
+	@RequestMapping("/error")
+	@ResponseBody
+
+	public String getErrorPath() {
+		return "No Mapping Found";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
